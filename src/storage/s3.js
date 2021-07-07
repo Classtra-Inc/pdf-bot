bbc0434042b2816d40579261b14b0fa979ff597a
ahmed-classtra <57930525+ahmed-classtra@users.noreply.github.com>
2021-07-07 10:53:53 -0400

    minor changes

--- src/storage/s3.js
+++ src/storage/s3.js
@@ -22,55 +22,56 @@ function createS3Storage(options = {}) {
 
   return (localPath, job) => {
     console.log('options: ', options)
-    return new Promise((resolve, reject) => {
-      console.log('options: ', options)
-      var awsS3Clientclient = new AWS.S3({
-              accessKeyId: options.accessKeyId,
-              secretAccessKey: options.secretAccessKey,
+    let promise = new Promise((resolve, reject) => {
+        console.log('options: ', options)
+        var awsS3Client = new AWS.S3({
+                accessKeyId: options.accessKeyId,
+                secretAccessKey: options.secretAccessKey,
+                region: options.region,
+              })
+        var options = {
+          s3Client: awsS3Client
+        }
+        
+        var client = s3.createClient(options)
+  
+        var remotePath = (options.path || '')
+        if (typeof options.path === 'function') {
+          remotePath = options.path(localPath, job)
+        }
+  
+        var pathSplitted = localPath.split('/')
+        var fileName = pathSplitted[pathSplitted.length - 1]
+        var fullRemotePath = path.join(remotePath, fileName)
+  
+        var uploadOptions = {
+          localFile: localPath,
+          defaultContentType: "application/pdf",
+  
+          s3Params: {
+            Bucket: options.bucket,
+            Key: fullRemotePath,
+            ContentType: "application/pdf"
+          },
+        }
+  
+        debug('Pushing job ID %s to S3 path: %s/%s', job.id, options.bucket, fileName)
+  
+        var uploader = client.uploadFile(uploadOptions);
+        uploader.on('error', function(err) {
+          reject(err)
+        });
+        uploader.on('end', function(data) {
+          resolve({
+            path: {
+              bucket: uploadOptions.s3Params.Bucket,
               region: options.region,
-            })
-      var options = {
-        s3Client: awsS3Client
-      }
-      
-      var client = s3.createClient(options)
-
-      var remotePath = (options.path || '')
-      if (typeof options.path === 'function') {
-        remotePath = options.path(localPath, job)
-      }
-
-      var pathSplitted = localPath.split('/')
-      var fileName = pathSplitted[pathSplitted.length - 1]
-      var fullRemotePath = path.join(remotePath, fileName)
-
-      var uploadOptions = {
-        localFile: localPath,
-        defaultContentType: "application/pdf",
-
-        s3Params: {
-          Bucket: options.bucket,
-          Key: fullRemotePath,
-          ContentType: "application/pdf"
-        },
-      }
-
-      debug('Pushing job ID %s to S3 path: %s/%s', job.id, options.bucket, fileName)
-
-      var uploader = client.uploadFile(uploadOptions);
-      uploader.on('error', function(err) {
-        reject(err)
-      });
-      uploader.on('end', function(data) {
-        resolve({
-          path: {
-            bucket: uploadOptions.s3Params.Bucket,
-            region: options.region,
-            key: uploadOptions.s3Params.Key
-          }
-        })
-      });
-    })
+              key: uploadOptions.s3Params.Key
+            }
+          })
+        });
+      })
+    return promise
   }
 }
 
